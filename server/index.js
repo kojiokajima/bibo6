69f389e5d789abf73299ce9041c7d74d146c2661
Koji Okajima <koji970124@gmail.com>
2021-08-03 16:17:04 -0700

    dleted a line

--- server/index.js
+++ server/index.js
@@ -15,7 +15,6 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, '../client/build')))
 
-
 // -------------Middlewares-------------
 app.use(authRouters);
 app.use(listRouters)
