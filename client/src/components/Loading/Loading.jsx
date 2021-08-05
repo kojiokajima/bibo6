8cf7f9ac8160d7d283584cf6567b95923264e970
Koji Okajima <koji970124@gmail.com>
2021-08-05 14:52:19 -0700

    deleted a package

--- client/src/components/Loading/Loading.jsx
+++ client/src/components/Loading/Loading.jsx
@@ -1,12 +1,8 @@
 import React from "react";
-import ReactLoading from "react-loading";
 import classes from "./Loading.module.scss";
 
 const Loading = () => {
-  return (
-    // <ReactLoading classNem={classes.root} type={"SpinningBubbles"} color={"#000"} />
-    <div className={classes.root}>Loading...</div>
-  );
+  return <div className={classes.root}>Loading...</div>;
 };
 
 export default Loading;
